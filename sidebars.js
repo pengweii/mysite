96642f8114d7ba9bbcb1f5ea920a34e6528126c8
Peng Wei <mr.pengwei@outlook.com>
2021-02-28 23:23:12 +0800

    add

--- sidebars.js
+++ sidebars.js
@@ -8,22 +8,31 @@
 //     Markdown语法: ["mdx"]
 //   }
 // };
-// 
+//
 module.exports = {
   docs: [
     {
-      type: 'category',
-      label: 'Guide',
+      type: "category",
+      label: "Guide",
       items: ["guide/doc1", "guide/装机配置"],
-    },{
-      type: 'category',
-      label: '前端基础',
+    },
+    {
+      type: "category",
+      label: "前端基础",
       items: ["前端基础/css"],
     },
     {
-      type: 'category',
-      label: 'Vue基础',
-      items: ["Vue基础/01", "Vue基础/02", "Vue基础/03", "Vue基础/04"]
+      type: "category",
+      label: "Vue基础",
+      items: [
+        "Vue基础/01",
+        "Vue基础/02",
+        "Vue基础/03",
+        "Vue基础/04",
+        "Vue基础/05",
+        "Vue基础/06",
+        "Vue基础/07",
+      ],
     },
-  ]
+  ],
 };
